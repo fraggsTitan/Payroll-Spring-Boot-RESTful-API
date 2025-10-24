24d7baa214ea0d6ca84a901d2fbb6901d8aa2862
fraggsTitan <vishnu.sara.kuma@gmail.com>
2025-10-24 16:10:05 +0530

    Added HATEOAS to handle and enhance HTTP responses

--- src/main/java/com/payroll/Payroll/EmployeeController.java
+++ src/main/java/com/payroll/Payroll/EmployeeController.java
@@ -1,24 +1,36 @@
 package com.payroll.Payroll;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class EmployeeController {
     private final EmployeeRepository employeeRepository;
-    EmployeeController(EmployeeRepository employeeRepository) {this.employeeRepository = employeeRepository;}
+    private final EmployeeModelAssembler employeeModelAssembler;
+    EmployeeController(EmployeeRepository employeeRepository,EmployeeModelAssembler  employeeModelAssembler)
+    {
+        this.employeeRepository = employeeRepository;
+        this.employeeModelAssembler = employeeModelAssembler;
+    }
     @GetMapping("/employees")
-    public List<Employee> findAll() {
-        return employeeRepository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+        List<EntityModel<Employee>>employees=employeeRepository.findAll().stream()
+                .map(employeeModelAssembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(employees,linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return employeeRepository.findById(id).orElseThrow(()->new EmployeeNotFoundException(id));
+    EntityModel<Employee>one(@PathVariable Long id){
+        Employee employee=employeeRepository.findById(id).orElseThrow(()->new EmployeeNotFoundException(id));
+        return employeeModelAssembler.toModel(employee);
     }
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
